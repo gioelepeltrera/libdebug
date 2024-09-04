e317d8805dd4f7cb3984c1a5aa5b98fa5d6491a6
gioelepeltrera <gioele.peltrera@mail.polimi.it>
2024-09-04 17:00:00 +0200

    Refactor class name to Arm64StackUnwinding

--- libdebug/architectures/arm64/arm64_stack_unwinding.py
+++ libdebug/architectures/arm64/arm64_stack_unwinding.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-class Aarch64StackUnwinding():
+class Arm64StackUnwinding():
     """
     Class that provides stack unwinding for the AArch64 architecture.
     """
