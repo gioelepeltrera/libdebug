52cf99f5b58f00496fd2fefe45dc6a638cb32655
gioelepeltrera <gioele.peltrera@mail.polimi.it>
2024-09-05 12:47:15 +0200

    Rewrittenr stack unwinding logic in Arm64StackUnwinding class

--- libdebug/architectures/arm64/arm64_stack_unwinding.py
+++ libdebug/architectures/arm64/arm64_stack_unwinding.py
@@ -14,6 +14,43 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+
+#class Arm64StackUnwinding():
+#    """
+#    Class that provides stack unwinding for the AArch64 architecture.
+#    """
+#
+#    def unwind(self, target: "Debugger") -> list:
+#        """
+#        Unwind the stack of a process.
+#
+#        Args:
+#            target (Debugger): The target Debugger.
+#        
+#        Returns:
+#            list: A list of return addresses.
+#        """
+#
+#        # Start with the current PC (Program Counter, equivalent to RIP in x86-64)
+#        current_fp = target.x29  # Frame pointer (x29)
+#        stack_trace = [target.pc]  # Program counter (x30 holds return address in AArch64)
+#
+#        while current_fp:
+#            try:
+#                # Read the return address from the link register (LR) or the stack
+#                return_address = int.from_bytes(target.memory[current_fp + 8, 8], byteorder="little")
+#
+#                # Read the previous frame pointer (x29) and set it as the current one
+#                current_fp = int.from_bytes(target.memory[current_fp, 8], byteorder="little")
+#
+#                # Append the return address to the stack trace
+#                stack_trace.append(return_address)
+#            except OSError:
+#                # If we hit an error while reading memory, stop unwinding
+#                break
+#
+#        return stack_trace
+#
 class Arm64StackUnwinding():
     """
     Class that provides stack unwinding for the AArch64 architecture.
@@ -27,25 +64,32 @@ class Arm64StackUnwinding():
             target (Debugger): The target Debugger.
         
         Returns:
-            list: A list of return addresses.
+            list: A list of return addresses (stack trace).
         """
 
-        # Start with the current PC (Program Counter, equivalent to RIP in x86-64)
-        current_fp = target.x29  # Frame pointer (x29)
-        stack_trace = [target.pc]  # Program counter (x30 holds return address in AArch64)
+        # Start with the current PC (Program Counter), add it to the stack trace
+        stack_trace = [target.pc]  # PC is the current instruction pointer
+
+        # If valid, add the return address from the link register (x30)
+        if target.x30 and target.x30 != 0:
+            stack_trace.append(target.x30)
+
+        # Use the frame pointer (x29) to unwind through the stack frames
+        current_fp = target.x29
 
         while current_fp:
             try:
-                # Read the return address from the link register (LR) or the stack
+                # Read the return address from the current stack frame (located at current_fp + 8)
                 return_address = int.from_bytes(target.memory[current_fp + 8, 8], byteorder="little")
-
-                # Read the previous frame pointer (x29) and set it as the current one
-                current_fp = int.from_bytes(target.memory[current_fp, 8], byteorder="little")
 
                 # Append the return address to the stack trace
                 stack_trace.append(return_address)
+
+                # Read the previous frame pointer (located at current_fp)
+                current_fp = int.from_bytes(target.memory[current_fp, 8], byteorder="little")
+
             except OSError:
-                # If we hit an error while reading memory, stop unwinding
+                # If there is an error while reading memory (invalid address), stop unwinding
                 break
 
         return stack_trace
