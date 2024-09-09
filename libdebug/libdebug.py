b39ed42d2f20966cfcdae1377fc287ed4bac2981
gioelepeltrera <gioele.peltrera@mail.polimi.it>
2024-09-09 11:43:20 +0200

    dont flush test after hitting watchpoint

--- libdebug/libdebug.py
+++ libdebug/libdebug.py
@@ -292,7 +292,6 @@ class Debugger:
             liblog.debugger("Stopped at %x but no breakpoint set, continuing", self.rip)
             print("Stopped at %x but no breakpoint set, continuing", self.rip)
             print("Value in registers:")
-            regs = [self.x0, self.x1, self.x2, self.x3, self.x4, self.x5, self.x6, self.x7, self.x8]
             for reg in regs:
                 print(hex(reg))
                 if reg in self.breakpoints:
@@ -302,7 +301,8 @@ class Debugger:
                     if breakpoint._callback:
                         breakpoint._callback(self, breakpoint)
                         self._empty_queue()
-                    self._flush_and_cont_after_bp(breakpoint)
+                    self.interface.continue_after_breakpoint(breakpoint)
+                    #self._flush_and_cont_after_bp(breakpoint)
                     return True
             self._flush_and_cont()
         print("End of _poll_and_run_on_process____")
